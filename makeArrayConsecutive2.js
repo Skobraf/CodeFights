f76a691c194ae4139a3c88a6121215f38efbe00f
Ayoub <skobraf@gmail.com>
2018-06-27 19:24:34 +0100

    added makeArrayConsecutive2

---
+++ makeArrayConsecutive2.js
@@ -0,0 +1,16 @@
+function makeArrayConsecutive2(statues) {
+var tab = statues.sort();
+    /*
+    var n=0;
+    for(var i =0; i<tab.length-1 ; i++){
+        if(tab[i+1]-tab[i] !=1)
+          n = n+ tab[i+1]-tab[i]-1;
+    }
+    return n;
+    */
+   
+   statues=statues.sort((a,b)=>a-b);
+   return(statues[statues.length-1]-statues[0]+1-statues.length);
+
+   
+}
